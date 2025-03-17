7c0ff1028e81d83deb2b0f68ae595a80b7bf46fa
ss398056 <ss398056@gmail.com>
2025-03-17 20:25:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ import express from 'express'
 
 app.use(express.json())
 app.use(cors({
-  origin: "*",
+  origin: ['http://localhost:5173','http://localhost:5000'],
+  credentials: true
 }))
 app.use(cookieParser())
 
