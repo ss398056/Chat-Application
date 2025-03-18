62cf1cd3d0ef9f2c3d29b5a454a65be60e526b2b
ss398056 <ss398056@gmail.com>
2025-03-18 17:47:24 +0530

    Update axios.instance.js

--- client/src/utilities/axios.instance.js
+++ client/src/utilities/axios.instance.js
@@ -4,7 +4,7 @@ const baseURL = import.meta.env.VITE_BASE_URL;
 console.log(baseURL)
 export const axiosInstance = axios.create({
     baseURL: baseURL,
-    withCredentials : true,
+    credentials: "include",
     headers: {
         "ContentType" : "application/json"
     }
