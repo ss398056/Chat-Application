a850ba48c0a3f7585692bb6e852216c92ff988be
ss398056 <ss398056@gmail.com>
2025-03-18 19:15:12 +0530

    update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -37,7 +37,9 @@ export const register = asyncHandler(async (req, res, next) => {
     .status(200)
     .cookie("token", token, {
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //7days expire
-      
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
@@ -76,7 +78,9 @@ export const login = asyncHandler(async (req, res, next) => {
     .status(200)
     .cookie("token", token, {
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //7days expire
-      
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
