17178c2f6bde580fddf64d5ad33e166bc9e92750
ss398056 <ss398056@gmail.com>
2025-03-18 19:28:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cors({
   origin: [
     "https://chat-application-git-main-sandeeps-projects-cb877a64.vercel.app",
-    "http://localhost:5173"
+    "http://localhost:5174"
   ],
   credentials: true,
 }));
