8b40921b1f15df24c3321dd03eb787c5b3be91a7
ss398056 <ss398056@gmail.com>
2025-03-19 17:41:31 +0530

    Update socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,40 +1,41 @@
-import {Server} from 'socket.io';
-import http from 'http';
-import express from 'express'
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:5173'
-    }
+  cors: {
+    origin: ["https://chat-application-orcin-zeta.vercel.app"],
+    credentials: true,
+  },
 });
 
 const userSocketMap = {
-    //userId : socketId     
-}
+  //userId : socketId
+};
 
-io.on("connection", (socket)=>{
-    //console.log(socket.id)
-    const userId = socket?.handshake?.query.userId;
-    
-    if(!userId) return;
-    
-    console.log(Object.keys(userSocketMap))
+io.on("connection", (socket) => {
+  //console.log(socket.id)
+  const userId = socket?.handshake?.query.userId;
 
-    userSocketMap[userId] = socket.id;
+  if (!userId) return;
 
-    io.emit("onlineUsers", Object.keys(userSocketMap))
+  console.log(Object.keys(userSocketMap));
 
-    socket.on("disconnect",()=>{
-        delete userSocketMap[userId];
-        io.emit("onlineUsers",Object.keys(userSocketMap))
-    })
-})
+  userSocketMap[userId] = socket.id;
 
-const getSocketId = (userId)=>{
-    return userSocketMap[userId];
-}
+  io.emit("onlineUsers", Object.keys(userSocketMap));
 
-export {io, app, server, getSocketId}
+  socket.on("disconnect", () => {
+    delete userSocketMap[userId];
+    io.emit("onlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+const getSocketId = (userId) => {
+  return userSocketMap[userId];
+};
+
+export { io, app, server, getSocketId };
